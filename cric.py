83616c99933bdb0bdda63ef93c8c57f33d2c954c
thiyagab <1@thiyagab.com>
2022-07-25 13:39:50 +0530

    Telegram integrated and validations added

--- cric.py
+++ cric.py
@@ -1,32 +1,54 @@
 import requests
 from bs4 import BeautifulSoup
+from deta import Deta
+
+import os
+projectkey=os.getenv('project_key').strip()
 
 URL = "https://m.cricbuzz.com/"
 filter_items = ['<b>FOUR</b>', '<b>SIX</b>', '<b>out</b>']
+deta = Deta(projectkey)
+db = deta.Base("crichighlights")
 
+
+# Store the last commentary text to check in next run
+# For first run and also subsequent runs, check all latest commentaries until the last saved commentary
+# Check for the pattern 'out', 'four', 'six', if in the latest commentaries you find any of these pattern,
+
+#TODO The same commentary will be updated, e.g. if someone is out, the same commentary line will be updated with more info,resulting in duplicate messages
+#TODO find ways to send commentary only for india matches
+#TODO look for patterns and try to improve it further
+#TODO add formatting to the messages
 
 def check_highlights():
     page = requests.get(URL)
-
     soup = BeautifulSoup(page.content, "html.parser")
-
     cricket_scores = soup.find_all(attrs={'class': 'cb-list-item'})
-
     if len(cricket_scores) > 1:
         commentaryurl = URL + cricket_scores[2].attrs['href']
         page = requests.get(commentaryurl)
         soup = BeautifulSoup(page.content, "html.parser")
         commentaries = soup.find_all(attrs={'class': 'commtext'})
+        count=0
         for commentary in commentaries:
+            last_commentary = get_last_commentary()
+            if(count==0):
+                put_last_commentary(commentary.text)
+            count=count+1
+            if last_commentary == commentary.text:
+                break
             for filter in filter_items:
                 if filter in str(commentary):
-                    print(commentary)
-                    break
+                    return commentary.text
 
-# Run this in a cron job every 1 minute
-# Store the last commentary text to check in next run
-# For first run and also subsequent runs, check all latest commentaries until the last saved commentary
-# Check for the pattern 'out', 'four', 'six', if in the latest commentaries you find any of these pattern,
-# send notification to telegram channel
+def put_last_commentary(commentary):
+    db.put(key='lastcommentary',data=commentary)
+
+def get_last_commentary():
+    return db.get(key='lastcommentary')
+
+
+
+
 
 # print(job_elements)
--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from deta import app
+import telegram
 
 import cric
+import os
+TELEGRAM_TOKEN = os.getenv('telegram_token').strip()
+TELEGRAM_CHAT_ID = os.getenv('my_telegram_id').strip()
 
 # define a function to run on a schedule
 # the function must take an event as an argument
 @app.lib.cron()
 def cron_job(event):
-    cric.check_highlights()
+    message=cric.check_highlights()
+    if message:
+        send_message(message)
     return "running on a schedule"
+
+def send_message(message):
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    bot.sendMessage(chat_id=TELEGRAM_CHAT_ID, text=message)
+    return "message sent"
